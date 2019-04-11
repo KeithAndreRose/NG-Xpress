d9d4438266840a8ee3930671b507f05d3287c8ea
Keith <andrerose722@gmail.com>
2019-04-11 07:58:50 -0400

    secondary nav, main wrapper, and footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { SecondaryNavigationComponent } from './secondary-navigation/secondary-navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    SecondaryNavigationComponent
   ],
   imports: [
     BrowserModule,
